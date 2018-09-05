c9711f510a4d61734155031bf89bb8c8b5d9e458
i'm batman <35092918+mr-robot0@users.noreply.github.com>
2018-09-06 00:36:37 +0530

    Done]

--- face_detect.py
+++ face_detect.py
@@ -4,22 +4,26 @@ global pt
 import os
 
 cam = cv2.VideoCapture(0)
-temp = cv2.imread(os.path.join(os.getcwd(),'templates\\temp.png'),0)
 font = cv2.FONT_HERSHEY_SIMPLEX
+
+temp_array = os.listdir("templates//")
+
+temps = {a.replace('.png',''):cv2.imread('templates//{}'.format(a),0) for a in temp_array}
 
 while True:
     gray = cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2GRAY)
-    w,h = temp.shape[::-1]
-    res = cv2.matchTemplate(gray,temp,cv2.TM_CCOEFF_NORMED)
-    thres = 0.7
+    for t in temps:
+        w,h = temps[t].shape[::-1]
+        res = cv2.matchTemplate(gray,temps[t],cv2.TM_CCOEFF_NORMED)
+        thres = 0.7
 
-    loc = np.where(res>=thres)
-    try:
-        pt = max(list(zip(*loc[::-1]))[:-10])
-        cv2.rectangle(gray,pt,(pt[0]+w , pt[1]+h),(255,255,255),1)
-        cv2.putText(gray,'Viraj',(pt[0],pt[1]), font, 3, (255,255,255), 1, cv2.LINE_AA)
-    except:
-        pass
+        loc = np.where(res>=thres)
+        try:
+            pt = max(list(zip(*loc[::-1]))[:-10])
+            cv2.rectangle(gray,pt,(pt[0]+w , pt[1]+h),(255,255,255),1)
+            cv2.putText(gray,t,(pt[0],pt[1]), font, 2, (255,255,255), 1, cv2.LINE_AA)
+        except:
+            pass
     
     cv2.imshow('',gray)
 
